438f9889e5c1971b6a61a815e3e40a42612be867
Gricel <63869574+Gricel-lee@users.noreply.github.com>
2021-05-11 00:40:19 +0200

    Create transitiveClosure2.pm

---
+++ Hospital-Example/5 Probab models/Prisminstance2/transitiveClosure2.pm
@@ -0,0 +1,35 @@
+mdp 
+
+label "r4_0_done" = (r4_0_at3_0=true) ;
+label "r3_0_done" = (r3_0_at3_0=true) ;
+
+label "jointtask_at3_0" = (r4_0 = 0) & (r3_0 = 0) ;
+
+module R4_0
+   //robotr4_0 has 1 tasks to schedule 
+   //task 1: at3_0
+   r4_0 : [0..1];
+   r4_0_at3_0 : bool;
+   // travel from state 0
+   [r4_0_cost0_1] r4_0=0 & r4_0_at3_0=false -> (r4_0' = 1) & (r4_0_at3_0'=true);
+   // travel from state 1
+   [r4_0_cost1_1] r4_0=1 & r4_0_at3_0=false -> (r4_0' = 1) & (r4_0_at3_0'=true);
+endmodule
+module R3_0
+   //robotr3_0 has 1 tasks to schedule 
+   //task 1: at3_0
+   r3_0 : [0..1];
+   r3_0_at3_0 : bool;
+   // travel from state 0
+   [r3_0_cost0_1] r3_0=0 & r3_0_at3_0=false -> (r3_0' = 1) & (r3_0_at3_0'=true);
+   // travel from state 1
+   [r3_0_cost1_1] r3_0=1 & r3_0_at3_0=false -> (r3_0' = 1) & (r3_0_at3_0'=true);
+endmodule
+
+
+rewards
+   [r4_0_cost0_1] true: 1;
+   [r4_0_cost1_1] true: 1;
+   [r3_0_cost0_1] true: 1;
+   [r3_0_cost1_1] true: 1;
+endrewards
