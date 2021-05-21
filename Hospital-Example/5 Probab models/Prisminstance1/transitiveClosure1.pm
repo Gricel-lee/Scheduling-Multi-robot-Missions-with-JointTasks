cd3245a7c7054b5b1d33bef9468f1e403dd05fbe
Gricel <63869574+Gricel-lee@users.noreply.github.com>
2021-05-21 18:15:30 +0200

    costs updated

--- Hospital-Example/5 Probab models/Prisminstance1/transitiveClosure1.pm
+++ Hospital-Example/5 Probab models/Prisminstance1/transitiveClosure1.pm
@@ -5,13 +5,13 @@ label "ct1_0" = (r1_0_at1_2 = true) | (r1_0_at2_2 = true) ;
 
 module R1_0
    //robotr1_0 has 7 tasks to schedule 
-   //task 1: at4_0
-   //task 2: at2_0
-   //task 3: at2_1
-   //task 4: at2_2
-   //task 5: at1_0
-   //task 6: at1_1
-   //task 7: at1_2
+   //state 1: task at4_0
+   //state 2: task at2_0
+   //state 3: task at2_1
+   //state 4: task at2_2
+   //state 5: task at1_0
+   //state 6: task at1_1
+   //state 7: task at1_2
    r1_0 : [0..7];
    r1_0_at4_0 : bool;
    r1_0_at2_0 : bool;
@@ -21,105 +21,105 @@ module R1_0
    r1_0_at1_1 : bool;
    r1_0_at1_2 : bool;
    // travel from state 0
-   [r1_0_cost0_1] r1_0=0 & r1_0_at4_0=false -> (r1_0' = 1) & (r1_0_at4_0'=true);
-   [r1_0_cost0_2] r1_0=0 & r1_0_at2_0=false -> (r1_0' = 2) & (r1_0_at2_0'=true);
-   [r1_0_cost0_3] r1_0=0 & r1_0_at2_1=false -> (r1_0' = 3) & (r1_0_at2_1'=true);
-   [r1_0_cost0_4] r1_0=0 & r1_0_at2_2=false -> (r1_0' = 4) & (r1_0_at2_2'=true);
-   [r1_0_cost0_5] r1_0=0 & r1_0_at1_0=false -> (r1_0' = 5) & (r1_0_at1_0'=true);
-   [r1_0_cost0_6] r1_0=0 & r1_0_at1_1=false -> (r1_0' = 6) & (r1_0_at1_1'=true);
-   [r1_0_cost0_7] r1_0=0 & r1_0_at1_2=false -> (r1_0' = 7) & (r1_0_at1_2'=true);
+   [r1_0_0_to_1] r1_0=0 & r1_0_at4_0=false -> (r1_0' = 1) & (r1_0_at4_0'=true);
+   [r1_0_0_to_2] r1_0=0 & r1_0_at2_0=false -> (r1_0' = 2) & (r1_0_at2_0'=true);
+   [r1_0_0_to_3] r1_0=0 & r1_0_at2_1=false -> (r1_0' = 3) & (r1_0_at2_1'=true);
+   [r1_0_0_to_4] r1_0=0 & r1_0_at2_2=false -> (r1_0' = 4) & (r1_0_at2_2'=true);
+   [r1_0_0_to_5] r1_0=0 & r1_0_at1_0=false -> (r1_0' = 5) & (r1_0_at1_0'=true);
+   [r1_0_0_to_6] r1_0=0 & r1_0_at1_1=false -> (r1_0' = 6) & (r1_0_at1_1'=true);
+   [r1_0_0_to_7] r1_0=0 & r1_0_at1_2=false -> (r1_0' = 7) & (r1_0_at1_2'=true);
    // travel from state 1
-   [r1_0_cost1_4] r1_0=1 & r1_0_at2_2=false -> (r1_0' = 4) & (r1_0_at2_2'=true);
-   [r1_0_cost1_7] r1_0=1 & r1_0_at1_2=false -> (r1_0' = 7) & (r1_0_at1_2'=true);
+   [r1_0_1_to_4] r1_0=1 & r1_0_at2_2=false -> (r1_0' = 4) & (r1_0_at2_2'=true);
+   [r1_0_1_to_7] r1_0=1 & r1_0_at1_2=false -> (r1_0' = 7) & (r1_0_at1_2'=true);
    // travel from state 2 - require state5(at1_0) to be done, otherwise go to 5
-   [r1_0_cost2_1] r1_0=2 & r1_0_at4_0=false & r1_0_at1_0=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
-   [r1_0_cost2_3] r1_0=2 & r1_0_at2_1=false & r1_0_at1_0=true -> (r1_0' = 3) & (r1_0_at2_1'=true);
-   [r1_0_cost2_4] r1_0=2 & r1_0_at2_2=false & r1_0_at1_0=true -> (r1_0' = 4) & (r1_0_at2_2'=true);
-   [r1_0_cost2_5] r1_0=2 & r1_0_at1_0=false & r1_0_at1_0=false -> (r1_0' = 5) & (r1_0_at1_0'=true);
-   [r1_0_cost2_6] r1_0=2 & r1_0_at1_1=false & r1_0_at1_0=true -> (r1_0' = 6) & (r1_0_at1_1'=true);
-   [r1_0_cost2_7] r1_0=2 & r1_0_at1_2=false & r1_0_at1_0=true -> (r1_0' = 7) & (r1_0_at1_2'=true);
+   [r1_0_2_to_1] r1_0=2 & r1_0_at4_0=false & r1_0_at1_0=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
+   [r1_0_2_to_3] r1_0=2 & r1_0_at2_1=false & r1_0_at1_0=true -> (r1_0' = 3) & (r1_0_at2_1'=true);
+   [r1_0_2_to_4] r1_0=2 & r1_0_at2_2=false & r1_0_at1_0=true -> (r1_0' = 4) & (r1_0_at2_2'=true);
+   [r1_0_2_to_5] r1_0=2 & r1_0_at1_0=false & r1_0_at1_0=false -> (r1_0' = 5) & (r1_0_at1_0'=true);
+   [r1_0_2_to_6] r1_0=2 & r1_0_at1_1=false & r1_0_at1_0=true -> (r1_0' = 6) & (r1_0_at1_1'=true);
+   [r1_0_2_to_7] r1_0=2 & r1_0_at1_2=false & r1_0_at1_0=true -> (r1_0' = 7) & (r1_0_at1_2'=true);
    // travel from state 3 - requires state 6(at1_1) to be done, otherwise go to 6
-   [r1_0_cost3_1] r1_0=3 & r1_0_at4_0=false & r1_0_at1_1=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
-   [r1_0_cost3_2] r1_0=3 & r1_0_at2_0=false & r1_0_at1_1=true -> (r1_0' = 2) & (r1_0_at2_0'=true);
-   [r1_0_cost3_4] r1_0=3 & r1_0_at2_2=false & r1_0_at1_1=true -> (r1_0' = 4) & (r1_0_at2_2'=true);
-   [r1_0_cost3_5] r1_0=3 & r1_0_at1_0=false & r1_0_at1_1=true -> (r1_0' = 5) & (r1_0_at1_0'=true);
-   [r1_0_cost3_6] r1_0=3 & r1_0_at1_1=false & r1_0_at1_1=false -> (r1_0' = 6) & (r1_0_at1_1'=true);
-   [r1_0_cost3_7] r1_0=3 & r1_0_at1_2=false & r1_0_at1_1=true -> (r1_0' = 7) & (r1_0_at1_2'=true);
+   [r1_0_3_to_1] r1_0=3 & r1_0_at4_0=false & r1_0_at1_1=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
+   [r1_0_3_to_2] r1_0=3 & r1_0_at2_0=false & r1_0_at1_1=true -> (r1_0' = 2) & (r1_0_at2_0'=true);
+   [r1_0_3_to_4] r1_0=3 & r1_0_at2_2=false & r1_0_at1_1=true -> (r1_0' = 4) & (r1_0_at2_2'=true);
+   [r1_0_3_to_5] r1_0=3 & r1_0_at1_0=false & r1_0_at1_1=true -> (r1_0' = 5) & (r1_0_at1_0'=true);
+   [r1_0_3_to_6] r1_0=3 & r1_0_at1_1=false & r1_0_at1_1=false -> (r1_0' = 6) & (r1_0_at1_1'=true);
+   [r1_0_3_to_7] r1_0=3 & r1_0_at1_2=false & r1_0_at1_1=true -> (r1_0' = 7) & (r1_0_at1_2'=true);
    // travel from state 4 - requires state 7(at1_2) to be done, otherwise go do 7
-   [r1_0_cost4_1] r1_0=4 & r1_0_at4_0=false & r1_0_at1_2=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
-   [r1_0_cost4_2] r1_0=4 & r1_0_at2_0=false & r1_0_at1_2=true -> (r1_0' = 2) & (r1_0_at2_0'=true);
-   [r1_0_cost4_3] r1_0=4 & r1_0_at2_1=false & r1_0_at1_2=true -> (r1_0' = 3) & (r1_0_at2_1'=true);
-   [r1_0_cost4_5] r1_0=4 & r1_0_at1_0=false & r1_0_at1_2=true -> (r1_0' = 5) & (r1_0_at1_0'=true);
-   [r1_0_cost4_6] r1_0=4 & r1_0_at1_1=false & r1_0_at1_2=true -> (r1_0' = 6) & (r1_0_at1_1'=true);
-   [r1_0_cost4_7] r1_0=4 & r1_0_at1_2=false & r1_0_at1_2=false -> (r1_0' = 7) & (r1_0_at1_2'=true);
+   [r1_0_4_to_1] r1_0=4 & r1_0_at4_0=false & r1_0_at1_2=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
+   [r1_0_4_to_2] r1_0=4 & r1_0_at2_0=false & r1_0_at1_2=true -> (r1_0' = 2) & (r1_0_at2_0'=true);
+   [r1_0_4_to_3] r1_0=4 & r1_0_at2_1=false & r1_0_at1_2=true -> (r1_0' = 3) & (r1_0_at2_1'=true);
+   [r1_0_4_to_5] r1_0=4 & r1_0_at1_0=false & r1_0_at1_2=true -> (r1_0' = 5) & (r1_0_at1_0'=true);
+   [r1_0_4_to_6] r1_0=4 & r1_0_at1_1=false & r1_0_at1_2=true -> (r1_0' = 6) & (r1_0_at1_1'=true);
+   [r1_0_4_to_7] r1_0=4 & r1_0_at1_2=false & r1_0_at1_2=false -> (r1_0' = 7) & (r1_0_at1_2'=true);
    // travel from state 5 - requires state 2(at2_0) to be done, otherwise go do 2
-   [r1_0_cost5_1] r1_0=5 & r1_0_at4_0=false & r1_0_at2_0=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
-   [r1_0_cost5_2] r1_0=5 & r1_0_at2_0=false & r1_0_at2_0=false -> (r1_0' = 2) & (r1_0_at2_0'=true);
-   [r1_0_cost5_3] r1_0=5 & r1_0_at2_1=false & r1_0_at2_0=true -> (r1_0' = 3) & (r1_0_at2_1'=true);
-   [r1_0_cost5_4] r1_0=5 & r1_0_at2_2=false & r1_0_at2_0=true -> (r1_0' = 4) & (r1_0_at2_2'=true);
-   [r1_0_cost5_6] r1_0=5 & r1_0_at1_1=false & r1_0_at2_0=true -> (r1_0' = 6) & (r1_0_at1_1'=true);
-   [r1_0_cost5_7] r1_0=5 & r1_0_at1_2=false & r1_0_at2_0=true -> (r1_0' = 7) & (r1_0_at1_2'=true);
+   [r1_0_5_to_1] r1_0=5 & r1_0_at4_0=false & r1_0_at2_0=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
+   [r1_0_5_to_2] r1_0=5 & r1_0_at2_0=false & r1_0_at2_0=false -> (r1_0' = 2) & (r1_0_at2_0'=true);
+   [r1_0_5_to_3] r1_0=5 & r1_0_at2_1=false & r1_0_at2_0=true -> (r1_0' = 3) & (r1_0_at2_1'=true);
+   [r1_0_5_to_4] r1_0=5 & r1_0_at2_2=false & r1_0_at2_0=true -> (r1_0' = 4) & (r1_0_at2_2'=true);
+   [r1_0_5_to_6] r1_0=5 & r1_0_at1_1=false & r1_0_at2_0=true -> (r1_0' = 6) & (r1_0_at1_1'=true);
+   [r1_0_5_to_7] r1_0=5 & r1_0_at1_2=false & r1_0_at2_0=true -> (r1_0' = 7) & (r1_0_at1_2'=true);
    // travel from state 6 - requires state 3(at2_1) to be done, otherwise go to 3
-   [r1_0_cost6_1] r1_0=6 & r1_0_at4_0=false & r1_0_at2_1=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
-   [r1_0_cost6_2] r1_0=6 & r1_0_at2_0=false & r1_0_at2_1=true -> (r1_0' = 2) & (r1_0_at2_0'=true);
-   [r1_0_cost6_3] r1_0=6 & r1_0_at2_1=false & r1_0_at2_1=false -> (r1_0' = 3) & (r1_0_at2_1'=true);
-   [r1_0_cost6_4] r1_0=6 & r1_0_at2_2=false & r1_0_at2_1=true -> (r1_0' = 4) & (r1_0_at2_2'=true);
-   [r1_0_cost6_5] r1_0=6 & r1_0_at1_0=false & r1_0_at2_1=true -> (r1_0' = 5) & (r1_0_at1_0'=true);
-   [r1_0_cost6_7] r1_0=6 & r1_0_at1_2=false & r1_0_at2_1=true -> (r1_0' = 7) & (r1_0_at1_2'=true);
+   [r1_0_6_to_1] r1_0=6 & r1_0_at4_0=false & r1_0_at2_1=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
+   [r1_0_6_to_2] r1_0=6 & r1_0_at2_0=false & r1_0_at2_1=true -> (r1_0' = 2) & (r1_0_at2_0'=true);
+   [r1_0_6_to_3] r1_0=6 & r1_0_at2_1=false & r1_0_at2_1=false -> (r1_0' = 3) & (r1_0_at2_1'=true);
+   [r1_0_6_to_4] r1_0=6 & r1_0_at2_2=false & r1_0_at2_1=true -> (r1_0' = 4) & (r1_0_at2_2'=true);
+   [r1_0_6_to_5] r1_0=6 & r1_0_at1_0=false & r1_0_at2_1=true -> (r1_0' = 5) & (r1_0_at1_0'=true);
+   [r1_0_6_to_7] r1_0=6 & r1_0_at1_2=false & r1_0_at2_1=true -> (r1_0' = 7) & (r1_0_at1_2'=true);
    // travel from state 7 - requires state 4(at2_2) to be done, otherwise go to 4
-   [r1_0_cost7_1] r1_0=7 & r1_0_at4_0=false & r1_0_at2_2=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
-   [r1_0_cost7_2] r1_0=7 & r1_0_at2_0=false & r1_0_at2_2=true -> (r1_0' = 2) & (r1_0_at2_0'=true);
-   [r1_0_cost7_3] r1_0=7 & r1_0_at2_1=false & r1_0_at2_2=true -> (r1_0' = 3) & (r1_0_at2_1'=true);
-   [r1_0_cost7_4] r1_0=7 & r1_0_at2_2=false & r1_0_at2_2=false -> (r1_0' = 4) & (r1_0_at2_2'=true);
-   [r1_0_cost7_5] r1_0=7 & r1_0_at1_0=false & r1_0_at2_2=true -> (r1_0' = 5) & (r1_0_at1_0'=true);
-   [r1_0_cost7_6] r1_0=7 & r1_0_at1_1=false & r1_0_at2_2=true -> (r1_0' = 6) & (r1_0_at1_1'=true);
+   [r1_0_7_to_1] r1_0=7 & r1_0_at4_0=false & r1_0_at2_2=true -> (r1_0' = 1) & (r1_0_at4_0'=true);
+   [r1_0_7_to_2] r1_0=7 & r1_0_at2_0=false & r1_0_at2_2=true -> (r1_0' = 2) & (r1_0_at2_0'=true);
+   [r1_0_7_to_3] r1_0=7 & r1_0_at2_1=false & r1_0_at2_2=true -> (r1_0' = 3) & (r1_0_at2_1'=true);
+   [r1_0_7_to_4] r1_0=7 & r1_0_at2_2=false & r1_0_at2_2=false -> (r1_0' = 4) & (r1_0_at2_2'=true);
+   [r1_0_7_to_5] r1_0=7 & r1_0_at1_0=false & r1_0_at2_2=true -> (r1_0' = 5) & (r1_0_at1_0'=true);
+   [r1_0_7_to_6] r1_0=7 & r1_0_at1_1=false & r1_0_at2_2=true -> (r1_0' = 6) & (r1_0_at1_1'=true);
 endmodule
 
 
 rewards
-   [r1_0_cost0_1] true: 141; //cost from initial position of robot 
-   [r1_0_cost0_2] true: 100; //cost from initial position of robot 
-   [r1_0_cost0_3] true: 111; //cost from initial position of robot 
-   [r1_0_cost0_4] true: 141; //cost from initial position of robot 
-   [r1_0_cost0_5] true: 100; //cost from initial position of robot 
-   [r1_0_cost0_6] true: 111; //cost from initial position of robot 
-   [r1_0_cost0_7] true: 141; //cost from initial position of robot 
-   [r1_0_cost1_4] true: 0;
-   [r1_0_cost1_7] true: 0;
-   [r1_0_cost2_1] true: 100;
-   [r1_0_cost2_3] true: 50;
-   [r1_0_cost2_4] true: 100;
-   [r1_0_cost2_5] true: 0;
-   [r1_0_cost2_6] true: 50;
-   [r1_0_cost2_7] true: 100;
-   [r1_0_cost3_1] true: 50;
-   [r1_0_cost3_2] true: 50;
-   [r1_0_cost3_4] true: 50;
-   [r1_0_cost3_5] true: 50;
-   [r1_0_cost3_6] true: 0;
-   [r1_0_cost3_7] true: 50;
-   [r1_0_cost4_1] true: 0;
-   [r1_0_cost4_2] true: 100;
-   [r1_0_cost4_3] true: 50;
-   [r1_0_cost4_5] true: 100;
-   [r1_0_cost4_6] true: 50;
-   [r1_0_cost4_7] true: 0;
-   [r1_0_cost5_1] true: 100;
-   [r1_0_cost5_2] true: 0;
-   [r1_0_cost5_3] true: 50;
-   [r1_0_cost5_4] true: 100;
-   [r1_0_cost5_6] true: 50;
-   [r1_0_cost5_7] true: 100;
-   [r1_0_cost6_1] true: 50;
-   [r1_0_cost6_2] true: 50;
-   [r1_0_cost6_3] true: 0;
-   [r1_0_cost6_4] true: 50;
-   [r1_0_cost6_5] true: 50;
-   [r1_0_cost6_7] true: 50;
-   [r1_0_cost7_1] true: 0;
-   [r1_0_cost7_2] true: 100;
-   [r1_0_cost7_3] true: 50;
-   [r1_0_cost7_4] true: 0;
-   [r1_0_cost7_5] true: 100;
-   [r1_0_cost7_6] true: 50;
+   [r1_0_0_to_1] true: 302; //cost from initial position of robot 
+   [r1_0_0_to_2] true: 176; //cost from initial position of robot 
+   [r1_0_0_to_3] true: 127; //cost from initial position of robot 
+   [r1_0_0_to_4] true: 302; //cost from initial position of robot 
+   [r1_0_0_to_5] true: 176; //cost from initial position of robot 
+   [r1_0_0_to_6] true: 127; //cost from initial position of robot 
+   [r1_0_0_to_7] true: 302; //cost from initial position of robot 
+   [r1_0_1_to_4] true: 0;
+   [r1_0_1_to_7] true: 0;
+   [r1_0_2_to_1] true: 400;
+   [r1_0_2_to_3] true: 100;
+   [r1_0_2_to_4] true: 400;
+   [r1_0_2_to_5] true: 0;
+   [r1_0_2_to_6] true: 100;
+   [r1_0_2_to_7] true: 400;
+   [r1_0_3_to_1] true: 412;
+   [r1_0_3_to_2] true: 100;
+   [r1_0_3_to_4] true: 412;
+   [r1_0_3_to_5] true: 100;
+   [r1_0_3_to_6] true: 0;
+   [r1_0_3_to_7] true: 412;
+   [r1_0_4_to_1] true: 0;
+   [r1_0_4_to_2] true: 400;
+   [r1_0_4_to_3] true: 412;
+   [r1_0_4_to_5] true: 400;
+   [r1_0_4_to_6] true: 412;
+   [r1_0_4_to_7] true: 0;
+   [r1_0_5_to_1] true: 400;
+   [r1_0_5_to_2] true: 0;
+   [r1_0_5_to_3] true: 100;
+   [r1_0_5_to_4] true: 400;
+   [r1_0_5_to_6] true: 100;
+   [r1_0_5_to_7] true: 400;
+   [r1_0_6_to_1] true: 412;
+   [r1_0_6_to_2] true: 100;
+   [r1_0_6_to_3] true: 0;
+   [r1_0_6_to_4] true: 412;
+   [r1_0_6_to_5] true: 100;
+   [r1_0_6_to_7] true: 412;
+   [r1_0_7_to_1] true: 0;
+   [r1_0_7_to_2] true: 400;
+   [r1_0_7_to_3] true: 412;
+   [r1_0_7_to_4] true: 0;
+   [r1_0_7_to_5] true: 400;
+   [r1_0_7_to_6] true: 412;
 endrewards
--- Hospital-Example/5 Probab models/Prisminstance1/transitiveClosure2.pm
+++ Hospital-Example/5 Probab models/Prisminstance1/transitiveClosure2.pm
@@ -11,7 +11,7 @@ module R4_0
    r4_0 : [0..1];
    r4_0_at3_0 : bool;
    // travel from state 0
-   [r4_0_cost0_1] r4_0=0 & r4_0_at3_0=false -> (r4_0' = 1) & (r4_0_at3_0'=true);
+   [r4_0_0_to_1] r4_0=0 & r4_0_at3_0=false -> (r4_0' = 1) & (r4_0_at3_0'=true);
 endmodule
 module R3_0
    //robotr3_0 has 1 task
@@ -19,10 +19,10 @@ module R3_0
    r3_0 : [0..1];
    r3_0_at3_0 : bool;
    // travel from state 0
-   [r3_0_cost0_1] r3_0=0 & r3_0_at3_0=false -> (r3_0' = 1) & (r3_0_at3_0'=true);
+   [r3_0_0_to_1] r3_0=0 & r3_0_at3_0=false -> (r3_0' = 1) & (r3_0_at3_0'=true);
 endmodule
 
 rewards
-   [r4_0_cost0_1] true: 100; //cost from initial position of robot 
-   [r3_0_cost0_1] true: 111; //cost from initial position of robot 
+   [r4_0_0_to_1] true: 127; //cost from initial position of robot 
+   [r3_0_0_to_1] true: 176; //cost from initial position of robot 
 endrewards
