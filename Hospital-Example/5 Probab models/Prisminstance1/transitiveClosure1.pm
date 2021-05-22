7db367dd84d5e1550998b85c4324248378b94f96
Gricel <63869574+Gricel-lee@users.noreply.github.com>
2021-05-23 01:55:53 +0200

    .

--- Hospital-Example/5 Probab models/Prisminstance1/transitiveClosure1.pm
+++ Hospital-Example/5 Probab models/Prisminstance1/transitiveClosure1.pm
@@ -70,24 +70,18 @@ rewards
    [r1_0_0_to_1] true: 302; //cost from initial position of robot 
    [r1_0_0_to_2] true: 176; //cost from initial position of robot 
    [r1_0_0_to_3] true: 127; //cost from initial position of robot 
-   [r1_0_0_to_4] true: 302; //cost from initial position of robot 
    [r1_0_0_to_5] true: 176; //cost from initial position of robot 
    [r1_0_0_to_6] true: 127; //cost from initial position of robot 
-   [r1_0_0_to_7] true: 302; //cost from initial position of robot 
    [r1_0_1_to_4] true: 0;
    [r1_0_1_to_7] true: 0;
    [r1_0_2_to_1] true: 400;
    [r1_0_2_to_3] true: 100;
-   [r1_0_2_to_4] true: 400;
    [r1_0_2_to_5] true: 0;
    [r1_0_2_to_6] true: 100;
-   [r1_0_2_to_7] true: 400;
    [r1_0_3_to_1] true: 412;
    [r1_0_3_to_2] true: 100;
-   [r1_0_3_to_4] true: 412;
    [r1_0_3_to_5] true: 100;
    [r1_0_3_to_6] true: 0;
-   [r1_0_3_to_7] true: 412;
    [r1_0_4_to_1] true: 0;
    [r1_0_4_to_2] true: 400;
    [r1_0_4_to_3] true: 412;
@@ -97,15 +91,11 @@ rewards
    [r1_0_5_to_1] true: 400;
    [r1_0_5_to_2] true: 0;
    [r1_0_5_to_3] true: 100;
-   [r1_0_5_to_4] true: 400;
    [r1_0_5_to_6] true: 100;
-   [r1_0_5_to_7] true: 400;
    [r1_0_6_to_1] true: 412;
    [r1_0_6_to_2] true: 100;
    [r1_0_6_to_3] true: 0;
-   [r1_0_6_to_4] true: 412;
    [r1_0_6_to_5] true: 100;
-   [r1_0_6_to_7] true: 412;
    [r1_0_7_to_1] true: 0;
    [r1_0_7_to_2] true: 400;
    [r1_0_7_to_3] true: 412;
